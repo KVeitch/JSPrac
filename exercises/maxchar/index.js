284069d8eb4b34b3d7c65a9dd1a0f770c0350bdc
Kirk Veitch <kirk.a.veitch@gmail.com>
2020-01-29 14:07:03 -0700

    Complete maxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,24 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+const maxChar = (str)=> {
+  let max = 0
+  let maxChar = ''
+  let charMap = [...str].reduce((acc,char)=>{
+    if(!acc[char]){
+      acc[char] = 0
+    }
+    acc[char]++
+    return acc
+  },{})
+
+  Object.keys(charMap).forEach(char =>{
+    if(charMap[char]> max){
+      max = charMap[char]
+      maxChar = char
+    }
+  })
+return maxChar
+}
 
 module.exports = maxChar;
