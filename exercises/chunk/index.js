28ee1f17a32ddcadc3d4d1bf6a7bd413a959f77e
Kirk Veitch <kirk.a.veitch@gmail.com>
2020-01-29 14:34:06 -0700

    Completes chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,15 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+const chunk = (array, size) => {
+  const chunks = [];
+  let index = 0;
+  while (index < array.length) {
+    chunks.push(array.slice(index, index + size));
+    index += size;
+  }
+  return chunks;
+
+}
 
 module.exports = chunk;
