b8075cba1570f795f62c43de678789a457d985a9
Kirk Veitch <kirk.a.veitch@gmail.com>
2020-03-09 14:33:11 -0600

    Refactor fizzBuzz

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -11,19 +11,29 @@
 //   fizz
 //   4
 //   buzz
+//Brute force
+// function fizzBuzz(n) {
+//   for (let i = 1; i <= n; i++) {
+//     if (i % 15 === 0 ) {
+//       console.log('fizzbuzz');
+//     } else if( i % 3 === 0) {
+//       console.log('fizz')
+//     } else if ( i % 5 === 0) {
+//       console.log('buzz')
+//     } else{
+//       console.log(i);
+//     }
+//   }
+// }
 
 function fizzBuzz(n) {
   for (let i = 1; i <= n; i++) {
-    if (i % 15 === 0 ) {
-      console.log('fizzbuzz');
-    } else if( i % 3 === 0) {
-      console.log('fizz')
-    } else if ( i % 5 === 0) {
-      console.log('buzz')
-    } else{
-      console.log(i);
-    }
+    let answer = '';
+    if (i % 3 === 0) {answer += 'fizz'}
+    if (i % 5 === 0) { answer += 'buzz'}
+    if (answer === '') { answer = i }
+    console.log(answer);
   }
-}
+};
 
 module.exports = fizzBuzz;
